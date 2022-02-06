e9f1dec626fb85fb9b1c8fc385862d56b5579931
felipe <felipemoreira1@gmail.com>
2022-02-06 18:46:49 -0300

    Stage 3/8: Palindromic numbers

--- Main.java
+++ Main.java
@@ -5,55 +5,53 @@ class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a natural number:");
-        String n = scanner.nextLine();
+        System.out.println("Welcome to Amazing Numbers!\n\n" +
+                "Supported requests:\n" +
+                "- enter a natural number to know its properties;\n" +
+                "- enter 0 to exit.");
 
-        if (!isNatural(n)) {
-            System.out.println("This number is not natural!");
+        long request = -1;
+        do {
+            System.out.println("\n\nEnter a request:");
+            request = scanner.nextLong();
 
-        } else {
-            int number = Integer.parseInt(n);
-            checkProperties(number);
-        }
+            if (!isNatural(request)) {
+                System.out.println("The first parameter should be a natural number or zero.");
+
+            } else if (request > 0) {
+                checkProperties(request);
+            }
+
+        } while (request != 0);
 
         scanner.close();
     }
 
-    public static boolean isNatural(String number) {
-        
-        if (number == null) {
-            return false;
-        }
+    public static boolean isNatural(long number) {
 
-        try {
-            int d = Integer.parseInt(number);
-            
-            return d > 0;
-
-        } catch (NumberFormatException nfe) {
-            return false;
-        }
+        return number >= 0;
     }
 
-    public static void checkProperties(int number) {
+    public static void checkProperties(long number) {
         boolean even = number % 2 == 0;
 
         System.out.printf("Properties of %d\n" +
-                            "\teven: %s\n" +
-                            "\todd: %s\n" +
-                            "\tbuzz: %s\n" +
-                            "\tduck: %s\n",
-                            number, even, !even, isBuzzNumber(number), isDuckNumber(number));
+                        "\teven: %s\n" +
+                        "\todd: %s\n" +
+                        "\tbuzz: %s\n" +
+                        "\tduck: %s\n" +
+                        "\tpalindromic: %s\n",
+                        number, even, !even, isBuzzNumber(number), isDuckNumber(number), isPalindrome(number));
     }
 
-    public static boolean isBuzzNumber(int number) {
-        
+    public static boolean isBuzzNumber(long number) {
+
         return (number % 10) == 7 || (number % 7) == 0;
 
     }
-    
-    public static boolean isDuckNumber(int number) {
-        
+
+    public static boolean isDuckNumber(long number) {
+
         while (number > 0) {
             if(number % 10 == 0) {
                 return true;
@@ -62,5 +60,18 @@ class Main {
         }
 
         return false;
+    }
+
+    public static boolean isPalindrome(long number) {
+        long n = number;
+        long reverse = 0;
+
+        while (n > 0) {
+            long lastDigit = n % 10;
+            reverse = reverse * 10 + lastDigit;
+            n /= 10;
+        }
+
+        return number == reverse;
     }
 }
