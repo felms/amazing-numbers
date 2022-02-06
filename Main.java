ea5e5cd2d6253b359ded5eea559be81bc8220998
felipe <felipemoreira1@gmail.com>
2022-02-06 18:20:11 -0300

    Stage 2/8: Duck numbers

--- Main.java
+++ Main.java
@@ -7,19 +7,14 @@ class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a natural number:");
         String n = scanner.nextLine();
-        
+
         if (!isNatural(n)) {
             System.out.println("This number is not natural!");
-            
+
         } else {
-
             int number = Integer.parseInt(n);
-
-            boolean isEven = number % 2 == 0;
-            System.out.printf("This number is %s.\n", isEven ? "Even" : "Odd");
-
-            isBuzzNumber(number);
-        } 
+            checkProperties(number);
+        }
 
         scanner.close();
     }
@@ -40,28 +35,32 @@ class Main {
         }
     }
 
-    public static void isBuzzNumber(int number) {
-        
-        int lastDigit = number % 10;
-        
-        boolean divisibleBy7 = number % 7 == 0;
+    public static void checkProperties(int number) {
+        boolean even = number % 2 == 0;
 
-        if(lastDigit == 7 || divisibleBy7) {
-            System.out.println("It is a Buzz number.\nExplanation:");
-            
-            if(lastDigit == 7 && divisibleBy7) {
-                System.out.printf("%d is divisible by 7 and ends with 7.\n", number);
-            } else if(divisibleBy7) {
-                System.out.printf("%d is divisible by 7.\n", number);
-            } else {
-                System.out.printf("%d ends with 7.\n", number);
+        System.out.printf("Properties of %d\n" +
+                            "\teven: %s\n" +
+                            "\todd: %s\n" +
+                            "\tbuzz: %s\n" +
+                            "\tduck: %s\n",
+                            number, even, !even, isBuzzNumber(number), isDuckNumber(number));
+    }
+
+    public static boolean isBuzzNumber(int number) {
+        
+        return (number % 10) == 7 || (number % 7) == 0;
+
+    }
+    
+    public static boolean isDuckNumber(int number) {
+        
+        while (number > 0) {
+            if(number % 10 == 0) {
+                return true;
             }
-
-        } else {
-            System.out.printf("It is not Buzz number.\n" + 
-                                "Explanation:\n" + 
-                                "%d is neither divisible by 7 nor does it end with 7.\n", 
-                                number);
+            number /= 10;
         }
+
+        return false;
     }
 }
